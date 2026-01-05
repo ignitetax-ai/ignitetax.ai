4f75c532d571cb7779581c3a8442bc21912ab175
pitzzahh <pitzzahh@gmail.com>
2026-01-05 16:55:38 +0800

    remove: unused highlighted text component

--- src/app/components/ui/index.ts
+++ src/app/components/ui/index.ts
@@ -2,5 +2,4 @@ export { default as SectionWrapper } from "./section-wrapper";
 export { default as SectionHeader } from "./section-header";
 export { default as FeatureCard } from "./feature-card";
 export { default as StatCard } from "./stat-card";
-export { default as HighlightedText } from "./highlighted-text";
 export { default as FeaturePill } from "./feature-pill";
